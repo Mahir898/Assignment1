01ed1988d454f401c151a8f2ee9601b0315a535d
Mahir Chowdhury <mahir.chowdhury2@wayne.edu>
2019-09-14 22:23:16 -0400

    Adding author name.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author : Mahir Chowdhury <mahir.chowdhury2@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
